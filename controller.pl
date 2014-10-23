2c90db4147aef044dcf77555833f966e733bda64
Magnus Jonsson <magnus@hpc2n.umu.se>
2014-10-23 16:55:47 +0200

    Added logging support.

--- controller.pl
+++ controller.pl
@@ -5,18 +5,22 @@ use warnings;
 
 use Carp;
 
+use Log::Log4perl;
+
 use lib 'lib';
 
 use MySensors;
-
 use MySensors::Backend::TXT;
+
+# Init logger
+Log::Log4perl->init("log.conf");
 
 my $backend = MySensors::Backend::TXT->new() || croak "Can't init Backend";
 
 my $mysensors = MySensors->new({ host => '192.168.2.10', 
                                  backend => $backend,
-								 debug => 1,
-							   }) || croak "Can't init MySensors";
+				 debug => 1,
+			   }) || croak "Can't init MySensors";
 
 $mysensors->connect() || croak "Can't connect to server";
 
