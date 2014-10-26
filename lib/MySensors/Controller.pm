deb248ef57e8f8af165a4c56a739ae8fcbc6a1de
Magnus Jonsson <magnus@hpc2n.umu.se>
2014-10-27 00:16:33 +0100

    Changed some vars/params for better names.

--- lib/MySensors/Controller.pm
+++ lib/MySensors/Controller.pm
@@ -30,7 +30,6 @@ sub setSocket {
 	return $self->{socket} = $socket;
 }
 
-
 # Send #
 
 # Send message
@@ -61,9 +60,9 @@ sub send {
 	return $self;
 }
 
-sub sendNextAvailableSensorId {
+sub sendNextAvailableNodeId {
 	my($self) = @_;
-	my($id) = $self->{backend}->getNextAvailableSensorId();
+	my($id) = $self->{backend}->getNextAvailableNodeId();
 	if(!defined $id) {
 		$self->{log}->error("Can't get new sensor id");
 		return;
@@ -78,18 +77,18 @@ sub sendNextAvailableSensorId {
 }
 
 sub sendValue {
-	my ($self,$sender,$sensor,$type) = @_;
-	$self->{log}->debug("Sender: $sender sensor: $sensor type: $type");
+	my ($self,$nodeid,$sensor,$type) = @_;
+	$self->{log}->debug("NodeID: $nodeid sensor: $sensor type: $type");
 	if(!$self->{backend}->can("getValue")) {
 		$self->{log}->error("Backend does not support 'getValue");
 		return;
 	}
-	my $value = $self->{backend}->getValue($sender,$sensor,$type);
+	my $value = $self->{backend}->getValue($nodeid,$sensor,$type);
 	if(!defined $value) {
 		$self->{log}->error("Got no value from backend");
 		return;
 	}
-	$self->send($sender,
+	$self->send($nodeid,
 			$sensor,
 			MySensors::Const::MessageType('SET'),
 			0,
@@ -98,9 +97,9 @@ sub sendValue {
 }
 
 sub sendTime {
-	my ($self,$sender,$sensor) = @_;
-	$self->{log}->debug("Sender: $sender sensor: $sensor");
-	$self->send($sender,
+	my ($self,$nodeid,$sensor) = @_;
+	$self->{log}->debug("NodeID: $nodeid sensor: $sensor");
+	$self->send($nodeid,
 			$sensor,
 			MySensors::Const::MessageType('INTERNAL'),
 			0,
@@ -134,60 +133,60 @@ sub sendVersionCheck {
 # SAVE #
 
 sub saveProtocol {
-	my($self,$sender,$protocol) = @_;
-	$self->{log}->debug("Sender: $sender protocol: $protocol");
+	my($self,$nodeid,$protocol) = @_;
+	$self->{log}->debug("NodeID: $nodeid protocol: $protocol");
 	if($self->{backend}->can("saveProtocol")) {
-		return $self->{backend}->saveProtocol($sender,$protocol);
+		return $self->{backend}->saveProtocol($nodeid,$protocol);
 	}
 }
 
 sub saveSensor {
-	my($self,$sender,$sensor,$type) = @_;
-	$self->{log}->debug("Sender: $sender sensor: $sensor type: $type");
+	my($self,$nodeid,$sensor,$type) = @_;
+	$self->{log}->debug("NodeID: $nodeid sensor: $sensor type: $type");
 	if($self->{backend}->can("saveSensor")) {
-		return $self->{backend}->saveSensor($sender,$sensor,$type);
+		return $self->{backend}->saveSensor($nodeid,$sensor,$type);
 	}
 }
 
 sub saveValue {
-	my ($self,$sender,$sensor,$type,$value) = @_;
-	$self->{log}->debug("Sender: $sender sensor: $sensor type: $type value: $value");
+	my ($self,$nodeid,$sensor,$type,$value) = @_;
+	$self->{log}->debug("NodeID: $nodeid sensor: $sensor type: $type value: $value");
 	if($self->{backend}->can("saveValue")) {
-		return $self->{backend}->saveValue($sender,$sensor,$type,$value);
+		return $self->{backend}->saveValue($nodeid,$sensor,$type,$value);
 	}
 }
 
 sub saveBatteryLevel {
-	my($self,$sender,$batteryLevel) = @_;
-	$self->{log}->debug("Sender: $sender batteryLevel $batteryLevel");
+	my($self,$nodeid,$batteryLevel) = @_;
+	$self->{log}->debug("NodeID: $nodeid batteryLevel $batteryLevel");
 	if($self->{backend}->can("saveBatteryLevel")) {
-		return $self->{backend}->saveBatteryLevel($sender,$batteryLevel);
+		return $self->{backend}->saveBatteryLevel($nodeid,$batteryLevel);
 	}
 }
 
 sub saveSketchName {
-	my($self,$sender,$name) = @_;
-	$self->{log}->debug("Sender: $sender name: $name");
+	my($self,$nodeid,$name) = @_;
+	$self->{log}->debug("NodeID: $nodeid name: $name");
 	if($self->{backend}->can("saveSketchName")) {
-		return $self->{backend}->saveSketchName($sender,$name);
+		return $self->{backend}->saveSketchName($nodeid,$name);
 	}
 }
 
 sub saveSketchVersion {
-	my($self,$sender,$version) = @_;
-	$self->{log}->debug("Sender: $sender version: $version");
+	my($self,$nodeid,$version) = @_;
+	$self->{log}->debug("NodeID: $nodeid version: $version");
 	if($self->{backend}->can("saveSketchVersion")) {
-		return $self->{backend}->saveSketchVersion($sender,$version);
+		return $self->{backend}->saveSketchVersion($nodeid,$version);
 	}
 }
 
 sub saveVersion {
-	my($self,$sender,$version) = @_;
+	my($self,$nodeid,$version) = @_;
 	$self->{log}->debug("Got version: $version");
-	if($sender == 0) {
+	if($nodeid == 0) {
 		$self->{version} = $version;
 		if($self->{backend}->can("saveVersion")) {
-			return $self->{backend}->saveVersion($sender,$version);
+			return $self->{backend}->saveVersion($nodeid,$version);
 		}
 	}
 }
@@ -195,7 +194,7 @@ sub saveVersion {
 # Other
 
 sub msg2str {
-	my($sender,$sensor,$command,$acknowledge,$type,$payload) = @_;
+	my($nodeid,$sensor,$command,$acknowledge,$type,$payload) = @_;
 	my $commandStr = MySensors::Const::MessageTypeToStr($command) . "($command)";
 	my $acknowledgeStr = ($acknowledge?'Ack':'NoAck') . "($acknowledge)";
 	my $typeStr = $type;
@@ -204,7 +203,7 @@ sub msg2str {
 												                 $command eq MySensors::Const::MessageType('SET');
 	$typeStr = MySensors::Const::InternalToStr($type)  . "($type)" if $command eq MySensors::Const::MessageType('INTERNAL');
 	$payload = "" unless defined $payload;
-	return join(" ; ","Sender:$sender","Sensor:$sensor",$commandStr,$acknowledgeStr,$typeStr,$payload);
+	return join(" ; ","NodeID:$nodeid","Sensor:$sensor",$commandStr,$acknowledgeStr,$typeStr,$payload);
 }
 
 sub process {
@@ -215,10 +214,10 @@ sub process {
 		$self->{log}->warn("Got no valid data: $data");
 		return;
 	}
-	my($sender,$sensor,$command,
+	my($nodeid,$sensor,$command,
 	   $acknowledge,$type,$payload) = ($1,$2,$3,$4,$5,$6);
 
-	$self->{log}->debug("Got: ", msg2str($sender,$sensor,$command,$acknowledge,$type,$payload));
+	$self->{log}->debug("Got: ", msg2str($nodeid,$sensor,$command,$acknowledge,$type,$payload));
 
 	$self->{lastMsg} = time;
 
@@ -228,28 +227,28 @@ sub process {
 	}
 	if($command == MySensors::Const::MessageType('PRESENTATION')) {
 		if($sensor == MySensors::Const::NodeSensorId()) {
-			$self->saveProtocol($sender,$payload);
+			$self->saveProtocol($nodeid,$payload);
 		}
-		$self->saveSensor($sender,$sensor,$type);
+		$self->saveSensor($nodeid,$sensor,$type);
 	} elsif($command == MySensors::Const::MessageType('SET')) {
-		$self->saveValue($sender,$sensor,$type,$payload);
+		$self->saveValue($nodeid,$sensor,$type,$payload);
 	} elsif($command == MySensors::Const::MessageType('REQ')) {
-		$self->sendValue($sender,$sensor,$type);
+		$self->sendValue($nodeid,$sensor,$type);
 	} elsif($command == MySensors::Const::MessageType('INTERNAL')) {
 		if($type == MySensors::Const::Internal('BATTERY_LEVEL')) {
-			$self->saveBatteryLevel($sender,$payload);
+			$self->saveBatteryLevel($nodeid,$payload);
 		} elsif($type == MySensors::Const::Internal('TIME')) {
-			$self->sendTime($sender, $sensor);
+			$self->sendTime($nodeid, $sensor);
 		} elsif($type == MySensors::Const::Internal('VERSION')) {
-			$self->saveVersion($sender, $payload);
+			$self->saveVersion($nodeid, $payload);
 		} elsif($type == MySensors::Const::Internal('ID_REQUEST')) {
-			$self->sendNextAvailableSensorId();
+			$self->sendNextAvailableNodeId();
 		} elsif($type == MySensors::Const::Internal('ID_RESPONSE')) {
 			# Do Nothing (Response to ID_REQUEST)
 		} elsif($type == MySensors::Const::Internal('INCLUSION_MODE')) {
 			# Do Nothing
 		} elsif($type == MySensors::Const::Internal('CONFIG')) {
-			$self->sendConfig($sender);
+			$self->sendConfig($nodeid);
 		} elsif($type == MySensors::Const::Internal('FIND_PARENT')) {
 			# Do Nothing
 		} elsif($type == MySensors::Const::Internal('FIND_PARENT_RESPONSE')) {
@@ -259,9 +258,9 @@ sub process {
 		} elsif($type == MySensors::Const::Internal('CHILDREN')) {
 			# Do Nothing
 		} elsif($type == MySensors::Const::Internal('SKETCH_NAME')) {
-			$self->saveSketchName($sender,$payload);
+			$self->saveSketchName($nodeid,$payload);
 		} elsif($type == MySensors::Const::Internal('SKETCH_VERSION')) {
-			$self->saveSketchVersion($sender,$payload);
+			$self->saveSketchVersion($nodeid,$payload);
 		} elsif($type == MySensors::Const::Internal('REBOOT')) {
 			# Do Nothing
 		} elsif($type == MySensors::Const::Internal('GATEWAY_READY')) {
@@ -290,6 +289,5 @@ sub timeoutCheck {
 	}
 	return 1;
 }
-
 
 1;
